d1e1900038e674ff2a2b229f8218e245441e3547
GabrielOliver07 <107689225+GabrielOliver07@users.noreply.github.com>
2022-11-25 00:30:32 -0300

    atualização das rotas

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -26,66 +26,57 @@ router.post('/', async function (req, res, next) {
     }
 
 })
-// retorna todos os projetos
-router.get('/', async (req, res, next) => {
-    try {
-      const commentFromDB = await Comment.find();
-      res.status(200).json(commentFromDB);
-    } catch (error) {    
-      next(error);
-    }
-  })
 
- // todos os comentarios de um manga especifico
- router.get('/:mangaId', async (req, res, next) => {
+// todos os comentarios de um manga especifico
+router.get('/:mangaId', async (req, res, next) => {
     const { mangaId } = req.params;
     try {
-      // valida se o projectId é um id que o mongo reconhece
-      if (!mongoose.Types.ObjectId.isValid(mangaId)) {
-        // return res.status(400).json({ message: 'Specified ID is not valid.'});
-        const error = new Error('Specified ID is not valid.');
-        error.status = 400;
-        // caso id não seja valido, jogamos um erro que vai para o catch
-        throw error; 
-      }
-      const mangaFromDB = await Comment.find({mangaId});
-      res.status(200).json(mangaFromDB);
-    } catch (error) {    
-      next(error);
+        // valida se o projectId é um id que o mongo reconhece
+        if (!mongoose.Types.ObjectId.isValid(mangaId)) {
+            // return res.status(400).json({ message: 'Specified ID is not valid.'});
+            const error = new Error('Specified ID is not valid.');
+            error.status = 400;
+            // caso id não seja valido, jogamos um erro que vai para o catch
+            throw error;
+        }
+        const mangaFromDB = await Comment.find({ mangaId });
+        res.status(200).json(mangaFromDB);
+    } catch (error) {
+        next(error);
     }
-  });
+});
 
 // edita um comentario específico
 router.put('/:commentId', async (req, res, next) => {
     const { commentId } = req.params;
     try {
-      if (!mongoose.Types.ObjectId.isValid(commentId)) {
-        const error = new Error('Specified ID is not valid.');
-        error.status = 400;
-        throw error; 
-      }
-      const commentFromDB = await Comment.findByIdAndUpdate(commentId, req.body, { new: true });
-      res.status(200).json(commentFromDB);
+        if (!mongoose.Types.ObjectId.isValid(commentId)) {
+            const error = new Error('Specified ID is not valid.');
+            error.status = 400;
+            throw error;
+        }
+        const commentFromDB = await Comment.findByIdAndUpdate(commentId, req.body, { new: true });
+        res.status(200).json(commentFromDB);
     } catch (error) {
-      next(error);
+        next(error);
     }
-  })
+})
 
-  // remove um comentario específico
+// remove um comentario específico
 router.delete('/:commentId', async (req, res, next) => {
     const { commentId } = req.params;
     try {
-      if (!mongoose.Types.ObjectId.isValid(commentId)) {
-        const error = new Error('Specified ID is not valid.');
-        error.status = 400;
-        throw error; 
-      }
-      const commentFromDB = await Comment.findByIdAndRemove(commentId);
-      res.status(204).json();
+        if (!mongoose.Types.ObjectId.isValid(commentId)) {
+            const error = new Error('Specified ID is not valid.');
+            error.status = 400;
+            throw error;
+        }
+        const commentFromDB = await Comment.findByIdAndRemove(commentId);
+        res.status(204).json();
     } catch (error) {
-      next(error);
+        next(error);
     }
-  })
+})
 
 
 module.exports = router
